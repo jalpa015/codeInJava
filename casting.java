2cc690209d9f718bb0472f7eda79519908002538
nysdas <nysdas@icloud.com>
2021-12-17 08:34:13 -0500

    casting

--- casting.java
+++ casting.java
@@ -3,6 +3,9 @@ public class casting {
     {
         int a = 10;
 
+        // byte b = a /10;
+        // result of this arithmetic will default to integer
+        // integer needs more space than byte. it will give error
         byte b = (byte) (a/10);
 
         System.out.println(b);
