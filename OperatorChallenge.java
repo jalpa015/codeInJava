53bba8384c72f4e3779a114c740a5fb559da6e73
nysdas <nysdas@icloud.com>
2021-12-17 21:26:12 -0500

    files

---
+++ OperatorChallenge.java
@@ -0,0 +1,14 @@
+public class OperatorChallenge {
+    public static void main(String[] args)
+    {
+        double myFirstvar = 20.00;
+        double mySecondVar = 80.00;
+
+        double result;
+
+        result = (myFirstvar + mySecondVar) * 100.00;
+        System.out.println("Result: " + result);
+
+    }
+    
+}
---
+++ conditionalLOGIC.java
@@ -0,0 +1,22 @@
+import java.lang.management.ThreadInfo;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class conditionalLOGIC {
+    public static void main(String[] args)
+    {
+        boolean isProgrammer = false;
+        
+
+        if (isProgrammer == true)
+        {
+            System.out.println("Yes, he is");
+        }
+        else
+        {
+            System.out.println("NO, he is NOT!");
+        }
+        
+        
+    }
+}
