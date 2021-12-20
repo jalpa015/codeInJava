1e1055823d4e3cf8bbc0ad4e576cb1bf978fc014
nysdas <nysdas@icloud.com>
2021-12-20 11:39:41 -0500

    Classes

---
+++ SevenSection/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Car nissan = new Car("Nissan",5000,2020, "Red" );
+
+
+        System.out.println(nissan.make);
+        System.out.println(nissan.color);
+        System.out.println(nissan.price);
+        System.out.println(nissan.year);
+    }
+    
+}
