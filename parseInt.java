d1b57962e57699fb4a54c7340096c23ce17305a0
nysdas <nysdas@icloud.com>
2021-12-19 23:50:11 -0500

    Convert_String_to_Integer

---
+++ parseInt.java
@@ -0,0 +1,12 @@
+public class parseInt {
+    public static void main( String[] args)
+    {
+        String numberAsString = "2018";
+        int number = Integer.parseInt(numberAsString);
+        
+        number ++;
+
+        System.out.println(number);
+    }
+    
+}
