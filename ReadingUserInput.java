1b3c55236e1d7eb8c2d6d1674484938b55647dbf
nysdas <nysdas@icloud.com>
2021-12-20 00:13:22 -0500

    Get_User_Input_as_INTEGER_NextLine_Method

--- ReadingUserInput.java
+++ ReadingUserInput.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class ReadingUserInput {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        
-        System.out.println("Your name is " + name);
 
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your Year of Birth: ");
         int BirthYear = scanner.nextInt();
         
         System.out.println("Your Year of Birth is " + BirthYear);
+
+        scanner.nextLine(); // handle next line character (enter key)
+        // without this, you won't get prompt for the name
+
+
+
+        
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Your name is " + name);
+
+        scanner.close();
+
+
+        
 
 
     }
